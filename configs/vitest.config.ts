bb14ffe3022e2f27c720b5899fc18e215e4347d3
Stijn Van Hulle <stijn.vanhulle@outlook.com>
2026-05-22 15:45:47 +0200

    Initial commit

---
+++ configs/vitest.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    testTimeout: 30_000,
+    exclude: ['**/node_modules/**', '**/dist/**', '**/mocks/**', '**/*.bench.ts'],
+    coverage: {
+      exclude: [
+        '**/dist/**',
+        '**/mocks/**',
+        '**/configs/**',
+        '**/scripts/**',
+        '**/index.ts',
+        '**/types.ts',
+        '**/tests/**',
+        '**/coverage/**',
+        '**/__snapshots__/**',
+        '**/packages/*/test?(s)/**',
+        '**/*.d.ts',
+        'test?(s)/**',
+        'test?(-*).?(c|m)[jt]s?(x)',
+        '**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)',
+        '**/__tests__/**',
+        '**/*.bench.ts',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsdown,build}.config.*',
+        '**/.rc.{?(c|m)js,yml}',
+      ],
+    },
+  },
+  resolve: {
+    tsconfigPaths: true,
+  },
+})
---
+++ packages/core/src/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { core } from './index.ts'
+
+test('Core', () => {
+  expect(core).toBeDefined()
+})
